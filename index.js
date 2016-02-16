fa11fa83959267b5500ae40827f40d48b314ca21
astur <astur@yandex.ru>
2016-02-16 04:28:33 +0300

    Basic functional

--- index.js
+++ index.js
@@ -2,6 +2,61 @@ function Tress(worker, concurrency){ // function worker(job, done)
 
     if(!(this instanceof Tress)) {return new Tress(worker, concurrency);}
 
+    var _concurrency = concurrency || 1;
+    var _paused = false;
+    var _queue = {
+        waiting: [],
+        running: [],
+    };
+    var _results = [];
+
+    var _jobDone = function(job){
+        return function(result){
+            _results.push(result);
+            var i = _queue.running.indexOf(job);
+            if (i > -1) {
+                _queue.running.splice(i, 1);
+            }
+            _startJob();
+        }
+    }
+
+    var _startJob = function(){
+        if (_paused || _queue.running.length === _concurrency || _queue.waiting.length === 0) return;
+        var job = _queue.waiting.shift();
+        _queue.running.push(job);
+        worker(job, _jobDone(job));
+        _startJob();
+    }
+
+    var _addJob = function(job, prior){
+        if (prior) {
+            _queue.waiting.unshift(job);
+        } else {
+            _queue.waiting.push(job);
+        }
+        _startJob();
+    }
+
+    this.push = function(job){
+        _addJob(job);
+    };
+
+    this.unshift = function(job){
+        _addJob(job, true);
+    };
+
+    this.pause = function(){
+        _paused = true;
+    };
+
+    this.resume = function(){
+        _paused = false;
+        _startJob();
+    };
+
+    Object.defineProperty(this, 'concurrency', { get: () => _concurrency });
+    Object.defineProperty(this, 'paused', { get: () => _paused });
 }
 
 module.exports = Tress;
