f7db13efa0f0e284df12ed12fc0032b22a480e46
astur <astur@yandex.ru>
2016-05-07 01:36:49 +0300

    new properties for job categories (instead 'queue' object)

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function Tress(worker, concurrency){ // function worker(job, done)
     var _started = false;
     var _queue = {
         waiting: [],
-        running: [],
-        errors: [],
+        active: [],
+        failed: [],
         finished: []
     };
 
@@ -20,20 +20,20 @@ function Tress(worker, concurrency){ // function worker(job, done)
     var _onSuccess = function(){};
 
     var _startJob = function(){
-        if(_queue.waiting.length === 0 && _queue.running.length === 0) _onDrain();
+        if(_queue.waiting.length === 0 && _queue.active.length === 0) _onDrain();
 
-        if (_paused || _queue.running.length === _concurrency || _queue.waiting.length === 0) return;
+        if (_paused || _queue.active.length === _concurrency || _queue.waiting.length === 0) return;
 
         var job = _queue.waiting.shift();
         if(_queue.waiting.length === 0) _onEmpty();
 
-        _queue.running.push(job);
-        if(_queue.running.length === _concurrency) _onSaturated();
+        _queue.active.push(job);
+        if(_queue.active.length === _concurrency) _onSaturated();
 
         worker(job.data, function(err){
-            _queue.running = _queue.running.filter((v) => v !== job);
-            _queue[err ? 'errors' : 'finished'].push(job);
-            if (_queue.running.length <= _concurrency - this.buffer) _onUnsaturated();
+            _queue.active = _queue.active.filter((v) => v !== job);
+            _queue[err ? 'failed' : 'finished'].push(job);
+            if (_queue.active.length <= _concurrency - this.buffer) _onUnsaturated();
             job.callback && job.callback.apply(job.data, arguments);
             err && _onError.apply(job.data, arguments);
             !err && _onSuccess.apply(job.data, Array.prototype.slice.call(arguments, 1));
@@ -73,9 +73,9 @@ function Tress(worker, concurrency){ // function worker(job, done)
     this.push = (job, callback) => _addJob(job, callback);
     this.unshift = (job, callback) => _addJob(job, callback, true);
     this.length = () => _queue.waiting.length;
-    this.running = () => _queue.running.length;
-    this.workersList = () => _queue.running;
-    this.idle = () => _queue.waiting.length + _queue.running.length === 0;
+    this.running = () => _queue.active.length;
+    this.workersList = () => _queue.active;
+    this.idle = () => _queue.waiting.length + _queue.active.length === 0;
     this.buffer = _concurrency / 4;
     this.pause = () => _paused = true;
     this.resume = () => {
@@ -87,16 +87,16 @@ function Tress(worker, concurrency){ // function worker(job, done)
         _queue.waiting = [];
     };
     this.save = (callback) => callback({
-        waiting: _queue.waiting.slice().concat(_queue.running),
-        errors: _queue.errors.slice(),
+        waiting: _queue.waiting.slice().concat(_queue.active),
+        failed: _queue.failed.slice(),
         finished: _queue.finished.slice()
     });
     this.load = (data) => {
         if (_started) throw new Error('Unable to load data after queue started');
         _queue = {
             waiting: data.waiting,
-            running: [],
-            errors: data.errors,
+            active: [],
+            failed: data.failed,
             finished: data.finished
         };
         !_paused && _startJob();
@@ -111,7 +111,10 @@ function Tress(worker, concurrency){ // function worker(job, done)
     Object.defineProperty(this, 'concurrency', { get: () => _concurrency });
     Object.defineProperty(this, 'paused', { get: () => _paused });
     Object.defineProperty(this, 'started', { get: () => _started });
-    Object.defineProperty(this, 'queue', { get: () => _queue });
+    Object.defineProperty(this, 'waiting', { get: () => _queue.waiting });
+    Object.defineProperty(this, 'active', { get: () => _queue.active });
+    Object.defineProperty(this, 'failed', { get: () => _queue.failed });
+    Object.defineProperty(this, 'finished', { get: () => _queue.finished });
 
 }
 
